1e8b3b80e7ffcd6f5af4007c3c9b50cc9a655089
Bruno-Suntak <brunosuntak@hotmail.com>
2023-11-29 21:18:43 -0300

    teste html-php concluido

--- html-php/processa_formulario.php
+++ html-php/processa_formulario.php
@@ -1,3 +1,20 @@
 <?php
-  // SEU CÓDIGO PHP AQUI
+  GetFormData();
+  function GetFormData(){
+
+    $nome = $_REQUEST["nome"];
+    $email = $_REQUEST["email"];
+
+    echo "Nome: $nome <br>";
+    echo "Email: $email";
+
+    if (empty($nome)) {
+      echo "<p style='color:red;'>Erro: O campo Nome não pode estar vazio.</p>";
+  }
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo "<p style='color:red;'>Erro: O campo Email deve ser um endereço de email válido.</p>";
+  }
+
+  }
 ?>
